1771ef711008f21a532121751ae9fe601efc1a4b
zhengjun <565061763@qq.com>
2022-07-10 23:31:04 +0800

    demo

--- app/src/main/java/com/umeng/soexample/wxapi/WXEntryActivity.kt
+++ app/src/main/java/com/umeng/soexample/wxapi/WXEntryActivity.kt
@@ -1,6 +1,11 @@
 package com.umeng.soexample.wxapi
 
 import com.sd.lib.social.wechat.activity.wxapi.FWechatCallbackActivity
+import com.tencent.mm.opensdk.modelbase.BaseResp
 
 class WXEntryActivity : FWechatCallbackActivity() {
+    override fun onResp(resp: BaseResp) {
+        super.onResp(resp)
+        finish()
+    }
 }
