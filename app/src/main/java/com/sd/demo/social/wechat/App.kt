01e940abf17fd45ffd10d75938e52b0d9e5a03eb
zhengjun <565061763@qq.com>
2022-07-11 17:19:50 +0800

    logMsg

--- app/src/main/java/com/sd/demo/social/wechat/App.kt
+++ app/src/main/java/com/sd/demo/social/wechat/App.kt
@@ -6,6 +6,7 @@ import com.sd.lib.social.wechat.FSocialWechat
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        FSocialWechat.init(this,"wxdc1e388c3822c80b", "3baf1193c85774b3fd9d18447d76cab0")
+        FSocialWechat.isDebug = true
+        FSocialWechat.init(this, "wxdc1e388c3822c80b", "3baf1193c85774b3fd9d18447d76cab0")
     }
 }
